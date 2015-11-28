6e98c576e8169c2104df3d241e1861dbbe980685
Mason Pierce <masopie@gmail.com>
2015-11-28 17:07:53 -0600

    Push gps stuff

---
+++ week-6/bingo_solution.rb
@@ -0,0 +1,54 @@
+# A Nested Array to Model a Bingo Board SOLO CHALLENGE
+
+# I spent [#] hours on this challenge.
+
+
+# Release 0: Pseudocode
+# Outline: We're going to have to create an array of arrays. One master array with 5 arrays, each with 5 elements. Each element of each array corresponds to a BINGO letter (ie indices are B = [0] ... O = [4]). The numbers therein will be randomly generated as part of our methods. So, it's essentially a 2D BINGO board. This is our BingoBoard class.
+
+# Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
+  #Input: none
+  #Output: rando letter+number combo
+  #Steps:
+
+# Check the called column for the number called.
+  #fill in the outline here
+
+# If the number is in the column, replace with an 'x'
+  #fill in the outline here
+
+# Display a column to the console
+  #fill in the outline here
+
+# Display the board to the console (prettily)
+  #fill in the outline here
+
+# Initial Solution
+
+class BingoBoard
+
+  def initialize(board)
+    @bingo_board = board
+  end
+
+
+
+end
+
+# Refactored Solution
+
+
+
+#DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
+
+board = [[47, 44, 71, 8, 88],
+        [22, 69, 75, 65, 73],
+        [83, 85, 97, 89, 57],
+        [25, 31, 96, 68, 51],
+        [75, 70, 54, 80, 83]]
+
+new_game = BingoBoard.new(board)
+
+
+#Reflection
+
