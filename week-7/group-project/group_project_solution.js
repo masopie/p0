2d599293d7c39e677bf0919e36d7bbbfb46e4d18
Mason Pierce <masopie@gmail.com>
2015-12-03 22:36:01 -0600

    Finish Person 1 of Group Challenge

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,6 @@
+/*
+As a user, I want to find the total of a list of numbers added together!
+As a user, I want to find the mean of a list of numbers!
+As a user, I want to find the median of a list of numbers!
+My list of numbers can be either an *even* number of numbers or an *odd* number of numbers!!
+*/
---
+++ week-7/nums_commas.js
@@ -0,0 +1,49 @@
+/*
+Your previous Ruby content is preserved below:
+
+// Separate Numbers with Commas in JavaScript **Pairing Challenge**
+
+
+// I worked on this challenge with: .
+
+// Pseudocode
+Input: an integer
+Output: a string
+Steps:
+- convert to a string
+- convert to array
+- reverse input
+- every third character, insert a comma
+- reverse the array
+- join array elements
+- convert element to a string
+*/
+// Initial Solution
+
+var separateComma = function(number) {
+  number = number.toString();
+  number = number.split("");
+  number = number.reverse();
+  //number = number.slice(2);
+
+  for (var counter = 3; counter < number.length; counter = counter + 4)
+  number.splice(counter, 3, ",")
+
+  console.log(number)
+
+//str.split([separator[, limit]])
+}
+separateComma(109285)
+
+
+// Refactored Solution
+
+
+
+
+// Your Own Tests (OPTIONAL)
+
+
+
+
+// Reflection
