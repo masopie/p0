b034f991428f18b8f80cf219ffbc54a532a5aaf5
Mason Pierce <masopie@gmail.com>
2015-12-05 22:08:02 -0600

    Do 7.5

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -4,7 +4,7 @@ Your previous Ruby content is preserved below:
 // Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
 
-// I worked on this challenge with: .
+// I worked on this challenge with: Steven Jones.
 
 // Pseudocode
 Input: an integer
@@ -19,7 +19,7 @@ Steps:
 - convert element to a string
 */
 // Initial Solution
-
+/*
 var separateComma = function(number) {
   number = number.toString();
   number = number.split("");
@@ -32,14 +32,16 @@ var separateComma = function(number) {
   number = number.join(""); // join() separates elements with commas by default. Hence the empty string as a parameter.
   console.log(number)
 }
-
+*/
 
 // Refactored Solution
 function separateComma(number) {
-
+  number = number.toString().split("").reverse();
+  for (var x = 3; x < number.length; x = x + 4)
+    number.splice(x, 0, ",");
+    number = number.reverse().join("");
+  console.log(number);
 }
-
-
 
 // Your Own Tests (OPTIONAL)
 
@@ -48,3 +50,15 @@ separateComma(30235982012394)
 
 
 // Reflection
+
+// What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+// Tough. The general, like, "thrust" of the syntax is VERY similar to Ruby (I've seen my friend using other lower-level languages and that's where the syntax is utterly alien). My pair and i just couldn't wrap our heads around how we'd iterate for an hour or more before just trying out the "for" expression.
+
+// What did you learn about iterating over arrays in JavaScript?
+// It feels a little more... wild west-y. You have to hammer out this whole "for" loop/expression instead of just calling a default method like #each in Ruby.
+
+// What was different about solving this problem in JavaScript?
+// It looks jankier, first of all. Just less clean than having a preset function like the #each method on Ruby.
+
+// What built-in methods did you find to incorporate in your refactored solution?
+// #toString, #splice, #split, #reverse. #splice was the big one, and we found it by mistake - my pair looked up "slice" and, lo and behold, I found "splice" trying to follow his research.
