86dec289ae14a491d37f96f89e0dc026905dd948
Mason Pierce <masopie@gmail.com>
2015-12-18 20:03:01 -0600

    Do 9.3.2

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -1,5 +1,19 @@
 // U3.W9:JQuery
 
+/*
+
+What is jQuery?
+I liked how Codecademy put it: jQuery is a library, or set of helpful add-ons, providing a simple interface for giving you immediate, visual, and REALLY POWERFUL results, instead of the more laborious (underlying) JS route.
+
+What does jQuery do for you?
+This is probably a good time to bring back up what a 'library' is (seemingly SOMEWHAT synonymous with 'framework'). A library, by the way -- it's just a file containing reusable code (objects/functions/methods) that you can link among multiple apps. jQuery is a library for JavaScript. You link it in its own JS file for respective HTML/CSS files to get access to easy and crazy-good movement, response, visual, etc. effects. As their tagline seems to go, 'The write less, do more, JS library.'
+
+What did you learn about the DOM while working on this challenge?
+Er... I kinda already did the DOM+JS challenge, but I can now see that Codecademy - again, sigh - has a great explanation:
+
+"The DOM consists of every element on the page, laid out in a hierarchical way that reflects the way the HTML document is ordered. Remember how we could think of the HTML document as a tree? You can think of the DOM the same way. Just as with an HTML document, elements in the DOM can have parents, children, and siblings."
+
+*/
 
 // I worked on this challenge [by myself, with: ].
 // This challenge took me [#] hours.
@@ -54,6 +68,10 @@ $('img').on('click', function(e){
 $('img').on('mouseleave', function(i){
   i.preventDefault()
   $(this).attr('src', 'dbc_logo.png')
+  /*$(this).animate({
+      opacity: 0.25,
+      height: "toggle"
+  }, 5000);*/
 })
 
 //RELEASE 5: Experiment on your own
@@ -66,6 +84,11 @@ $('img').on('mouseleave', function(i){
 ...Damn. Guess making a clicked button magically transmogrify into an image is going to be tougher than that...
 */
 
-animate
+$('img').click(function(){
+  $(this).animate({
+      opacity: 0.25,
+      height: "toggle"
+  }, 5000);
+});
 
 })  // end of the document.ready function: do not remove or write DOM manipulation below this.
